01e84fa51ee2e2b9ce2f1293c5c50376ff681c6d
Alex Banks <alex@moonhighway.com>
2016-03-12 13:34:17 -0800

    Initial Commit: App Constants defined and package.json created

---
+++ constants.js
@@ -0,0 +1,16 @@
+module.export = {
+
+    // Actions
+
+    SORT_COLORS: "SORT_COLORS",
+    ADD_COLOR: "ADD_COLOR",
+    RATE_COLOR: "RATE_COLOR",
+    REMOVE_COLOR: "REMOVE_COLOR",
+
+    // States
+
+    SORTED_BY_DATE: "SORTED_BY_DATE",
+    SORTED_BY_COLOR: "SORTED_BY_COLOR",
+    SORTED_BY_RATING: "SORTED_BY_RATING"
+
+};
