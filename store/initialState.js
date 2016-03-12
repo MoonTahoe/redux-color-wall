2f848a265a30e31d2565b890eafae74012bf125d
Alex Banks <alex@moonhighway.com>
2016-03-12 15:31:44 -0800

    Adds initial app state for test data

---
+++ store/initialState.js
@@ -0,0 +1,23 @@
+module.exports = {
+    colors: [
+        {
+            id: 0,
+            title: "Rad Red",
+            color: "#FF0000",
+            rating: 3
+        },
+        {
+            id: 0,
+            title: "Crazy Green",
+            color: "#00FF00",
+            rating: undefined
+        },
+        {
+            id: 0,
+            title: "Big BLue",
+            color: "#0000FF",
+            rating: 5
+        }
+    ],
+    sortBy: ""
+};
