b35d13058ac3a020dde85a9c6d7fa0cca72141cf
Alex Banks <alex@moonhighway.com>
2016-03-12 16:51:28 -0800

    Adds sort reducer and tests

--- store/reducers/sort.js
+++ store/reducers/sort.js
@@ -1,3 +1,12 @@
-const sort = (state="", action) => state;
+import C from '../../constants'
+
+const sort = (state=C.SORTED_BY_DATE, action={type:null}) => {
+    switch(action.type) {
+        case C.SORT_COLORS:
+            return action.sortBy;
+        default :
+            return state;
+    }
+};
 
 module.exports = sort;
--- test/store/reducers/sort-spec.js
+++ test/store/reducers/sort-spec.js
@@ -1,12 +1,21 @@
+import C from '../../../constants'
 import { sort } from '../../../store/reducers'
+import deepFreeze from 'deep-freeze'
 import { expect } from 'chai'
 
 describe("sort Reducer", () => {
 
-    it("Sorts by Date");
+    it("SORT_COLORS success", () => {
+        const state = {};
+        const action = {
+            type: C.SORT_COLORS,
+            sortBy: C.SORTED_BY_RATING
+        };
+        deepFreeze(state);
+        deepFreeze(action);
+        expect(sort(state, action)).to.equal(C.SORTED_BY_RATING);
+    });
 
-    it("Sorts by Color");
-
-    it("Sorts by Rating");
+    it("defaults to SORTED_BY_DATE", () => expect(sort()).to.equal(C.SORTED_BY_DATE));
 
 });
